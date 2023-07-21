97f4494a17d2c4c150f61f3f5deb32d374e2b3ae
Ryuichi <100734822+zakzackr@users.noreply.github.com>
2023-07-21 15:02:13 +0900

    Update streamFlatMap.java

--- streamFlatMap.java
+++ streamFlatMap.java
@@ -3,8 +3,11 @@ import java.util.Arrays;
 class Main{
     public static void main(String[] args){
         int[][] array2d = {{1, 2, 3, 4}, {10, 20, 30}, {100}};
-        // 2d -> 1d Array
-        int[] flatten = Arrays.stream(array2d).flatMapToInt(Arrays::stream).toArray();
+        
+        // Arrays.stream(array2d): 2dArrayをStream<int[]>に変換
+        // flatMapToInt(Arrays::stream): subArrayをIntStreamに変換
+        // toArray(): 2dArrayのそれぞれの要素からなるIntStreamをint[]に変換
+        int[] flatten = Arrays.stream(array2d).flatMapToInt(Arrays::stream).toArray();  // 2d -> 1d Array
         int sum = Arrays.stream(array2d).flatMapToInt(Arrays::stream).filter(x -> x > 10).reduce(0, (x, total) -> x + total);
 
         System.out.println(Arrays.toString(flatten));
@@ -12,3 +15,4 @@ class Main{
         System.out.println(sum);
     }
 }
+
